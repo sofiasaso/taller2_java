4a134932a404ade949738d7019d64f079b50b058
sofiasaso <121638526+sofiasaso@users.noreply.github.com>
2023-03-29 02:45:00 -0500

    Create Circulo.java

---
+++ src/Figuras/Circulo.java
@@ -0,0 +1,21 @@
+package Figuras;
+
+public class Circulo {
+
+    private double radio;
+
+    public void setRadio(float radio){
+        this.radio=radio;
+    }
+
+    public double getRadio(){
+        return radio;
+    }
+
+    public void calcularArea(){
+        double area=(radio*radio)* Math.PI;
+        System.out.println("\nEl área del circulo con un radio de " + this.radio + ", es igual a " + area);
+    }
+
+
+}
